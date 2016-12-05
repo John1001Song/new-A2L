6f1411c708bb5fcbea06dd953dda6ac8ad2eea45
Jack <dongtianzheng@gmail.com>
2016-12-04 19:21:12 -0500

    add course page

---
+++ js/coursePage.js
@@ -0,0 +1,37 @@
+// $(document).read(function() {
+//     $('.instructor-accordion-toggle').on('click', function(event) {
+//         event.preventDefault();
+
+//         var accordion = $(this);
+//         var accordionContent = accordion.next('instructor-accordion-content');
+
+
+//         accordion.toggleClass("open");
+//         accordionContent.slideToggle(250);
+//     });
+// });
+
+
+
+$(document).ready(function() {
+    $('.accordion-toggle').on('click', function(event) {
+        event.preventDefault();
+        // create accordion variables
+        var accordion = $(this);
+        var accordionContent = accordion.next('.accordion-content');
+
+        // toggle accordion link open class
+        accordion.toggleClass("open");
+        // toggle accordion content
+        accordionContent.slideToggle(250);
+
+    });
+
+
+    $(".content-row").click(function() {
+
+        // console.log($(this).data("href"));
+
+        window.document.location = $(this).data("href");
+    });
+});
